3784b57b7fa5e0bbd61e5612473d8950a6d27d33
SunRiver-NEU <sunriver_kun@qq.com>
2023-04-07 11:29:12 +0800

    全局变量_G

--- functions.js
+++ functions.js
@@ -1,49 +1,81 @@
-var widgetStack = [];
-exports.pushScreen = function (screen) {
-    widgetStack.push(screen);
-};
+var _G = Number.prototype._G;
+var filePath = "functions.js";
+if (_G.loadedFiles[filePath] == null) {
+    _G.loadedFiles[filePath] = _G;
+    print("Load " + filePath);
 
-exports.popScreen = function () {
-    return widgetStack.pop();
-};
+    var widgetStack = [];
 
-exports.topScreen = function () {
-    return widgetStack[widgetStack.length - 1];
-};
+    _G.getWidgetStack = function (){
+        return widgetStack;
+    };
 
-exports.clearScreen = function (){
-    for(var i=widgetStack.length - 1; i>=0; --i){
-      exports.hide(widgetStack[i]);
-      widgetStack.pop();
-    }
-};
+    _G.pushScreen = function (screen) {
+        print("_G.pushScreen: ", screen);
+        if (widgetStack.length > 0) {
+            _G.hide(widgetStack[widgetStack.length - 1]);
+        }
+        _G.show(screen);
+        widgetStack.push(screen);
+        Map.add(screen.widget);
+    };
 
-exports.show = function (ui) {
-    ui.style().set("shown", true);
-};
+    _G.popScreen = function () {
+        var screen = widgetStack.pop();
+        print("_G.popScreen: ", screen);
+        _G.hide(screen);
+        Map.remove(screen.widget);
+        if (widgetStack.length > 0) {
+            _G.show(widgetStack[widgetStack.length - 1]);
+        }
+        return screen;
+    };
 
-exports.hide = function (ui) {
-    ui.style().set("shown", false);
-};
+    _G.topScreen = function () {
+        return widgetStack[widgetStack.length - 1];
+    };
 
-exports.handler = function (self, fn) {
-    return function (params) { fn(self, params); };
-};
+    _G.clearScreen = function () {
+        for (var i = widgetStack.length - 1; i >= 0; --i) {
+            _G.hide(widgetStack[i]);
+            Map.remove(widgetStack[i].widget);
+            widgetStack.pop();
+        }
+    };
 
-exports.horizontals = function (widgets) {
-    var panel = ui.Panel();
-    panel.setLayout(ui.Panel.Layout.flow("horizontal"));
-    for(var i=0; i<widgets.length; ++i){
-      panel.add(widgets[i]);
-    }
-    return panel;
-}
+    _G.show = function (widget) {
+        if (widget.widget != null) { widget = widget.widget; }
+        widget.style().set("shown", true);
+    };
 
-exports.verticals = function (widgets) {
+    _G.hide = function (widget) {
+        if (widget.widget != null) { widget = widget.widget; }
+        widget.style().set("shown", false);
+    };
+
+    _G.handler = function (self, fn) {
+        return function (params) { fn(self, params); };
+    };
+
+    _G.horizontals = function (widgets) {
         var panel = ui.Panel();
-    panel.setLayout(ui.Panel.Layout.flow("vertical"));
-    for(var i=0; i<widgets.length; ++i){
-      panel.add(widgets[i]);
+        panel.setLayout(ui.Panel.Layout.flow("horizontal"));
+        for (var i = 0; i < widgets.length; ++i) {
+            panel.add(widgets[i]);
+        }
+        return panel;
     }
-    return panel;
+
+    _G.verticals = function (widgets) {
+        var panel = ui.Panel();
+        panel.setLayout(ui.Panel.Layout.flow("vertical"));
+        for (var i = 0; i < widgets.length; ++i) {
+            panel.add(widgets[i]);
+        }
+        return panel;
+    }
 }
+else{
+    
+}
+
--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+Number.prototype._G = {};    //全局变量   
+var _G = Number.prototype._G;
+_G.loadedFiles = {};    //加载过的文件列表，避免require时多次执行代码
+
+require("users/sunriverkun/gee_test:functions.js");
+
 var ToolBox = require("users/sunriverkun/gee_test:widgets/toolBox.js");
 var toolBox = ToolBox.new();
 toolBox.widget.style().set("position", "top-right");
