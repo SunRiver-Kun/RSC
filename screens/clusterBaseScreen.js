6bd55179684d75dbaa9563c0b2f49cd8c5be6a19
SunRiver-NEU <sunriver_kun@qq.com>
2023-05-04 12:46:07 +0800

    非监督分类大小限制

--- screens/clusterBaseScreen.js
+++ screens/clusterBaseScreen.js
@@ -6,6 +6,7 @@ if (_G.loadedFiles[filePath] == null) {
 
     var MapDrawer = require("users/sunriverkun/gee_test:widgets/mapDrawer.js");
     var ImageChooser = require("users/sunriverkun/gee_test:widgets/imageChooser.js");
+    var divCh = ";";
 
     exports.new = function (title, titleDes) {
         var panel = ui.Panel();
@@ -27,8 +28,11 @@ if (_G.loadedFiles[filePath] == null) {
         panel.add(self.titleLabel);
         panel.add(self.titleDesLabel);
         //图像输入
-        self.imageChooser = ImageChooser.new(false, function () { exports.setRegion(self, null); });
+        self.imageChooser = ImageChooser.new(false, _G.handler(self, exports.onSetImage));
+        self.bandsLabel = ui.Label("分类波段名");
+        self.bandsTex = ui.Textbox("以" + divCh + "分割每个波段名", "");
         panel.add(self.imageChooser.widget);
+        panel.add(_G.horizontals([self.bandsLabel, self.bandsTex]));
 
         //算法参数
         self.argsPanel = ui.Panel();
@@ -46,6 +50,8 @@ if (_G.loadedFiles[filePath] == null) {
         self.classButton = ui.Button("分类", _G.handler(self, exports.onClass));
         self.cancelButton = ui.Button("取消", function () { _G.popScreen(); });
         panel.add(_G.horizontals([self.classButton, self.cancelButton]));
+        
+        panel.add(ui.Label("当影像过大时，可以选择绘制训练区域和减少分类波段数。", _G.styles.des));
 
         ImageChooser.setImageName(self.imageChooser, "LANDSAT/LC08/C01/T1_SR/LC08_192024_20160624");
 
@@ -57,13 +63,25 @@ if (_G.loadedFiles[filePath] == null) {
         self.regionLabel.setValue(region == null ? "(全部)" : "(部分)");
     };
 
-    //abstract
-    exports.getClassifier = function (self) {
-        return null;
+    exports.onSetImage = function (self, name) {
+        exports.setRegion(self, null);
+        self.bandsTex.setValue(_G.getImageBands(name).join(divCh));
     };
 
     exports.getShowImage = function (self) {
         return ImageChooser.getShowImage(self.imageChooser);
+    };
+
+    exports.getBands = function (self) {
+        var str = self.bandsTex.getValue();
+        if(str == "") { return null; }
+        var bands = str.split(divCh);
+        for (var i = bands.length - 1; i >= 0; --i) {
+            if (bands[i] == "") {
+                bands.splice(i, 1);
+            }
+        }
+        return bands;
     };
 
     function union(a, b) {
@@ -80,11 +98,21 @@ if (_G.loadedFiles[filePath] == null) {
             for (var i = 0; i < length; ++i) {
                 area = union(area, geoLayers.get(length - 1).toGeometry());
             }
-            exports.setRegion(self, area); 
+            exports.setRegion(self, area);
         }, null, true);
     };
 
+    //abstract
+    exports.getClassifier = function (self) {
+        return null;
+    };
+
+    //optional
+
     exports.onClass = function (self) {
+        var bands = exports.getBands(self);
+        if (bands == null || bands.length == 0) { alert("未填写分类波段名称"); return; }
+
         var classifier = self.c.getClassifier(self);
         if (classifier == null) { return; }
 
@@ -92,15 +120,17 @@ if (_G.loadedFiles[filePath] == null) {
         if (image == null) { return; }
 
         print("image", image);
-        var training = image.sample({
+        var training = image.select(bands).sample({
             region: self.region,
             scale: 30,
-            numPixels: 5000
+            numPixels: 50
         });
         print("training", training);
-        var clusterer = classifier.train(training);
-        var result = image.cluster(clusterer).randomVisualizer();
+        var clusterer = classifier.train(training, bands);
+        var result = image.cluster(clusterer);
         print("result", result);
+
+        result = result.randomVisualizer();
         Map.addLayer(result, {}, self.title);
     };
 
